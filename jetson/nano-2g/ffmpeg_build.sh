cabda99c3874f17a6d18efe187584d479f908ac6
stoney <sikang99@gmail.com>
2021-02-16 23:22:04 +0900

    0.0.0.8,stoney

--- jetson/nano-2g/ffmpeg_build.sh
+++ jetson/nano-2g/ffmpeg_build.sh
@@ -1,4 +1,20 @@
+## Normal ffmpeg
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
+cd ffmpeg
 ./configure --disable-static --enable-shared --disable-doc
 make
+sudo make install
+sudo ldconfig
+
+
+## Hardware accelerated ffmpeg
+git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+cd nv-codec-headers && sudo make install && cd ..
+
+sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
+cd ffmpeg
+./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+make -j4
 sudo make install
 sudo ldconfig
