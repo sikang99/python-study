1fedb53e32b2dfe11651ed3329b1de798eedc6a2
stoney <sikang99@gmail.com>
2021-02-17 00:19:24 +0900

    0.0.0.8,stoney

--- jetson/nano-2g/ffmpeg_build.sh
+++ jetson/nano-2g/ffmpeg_build.sh
@@ -14,7 +14,7 @@ cd nv-codec-headers && sudo make install && cd ..
 sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
 cd ffmpeg
-./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
+./configure --enable-ffplay --enable-shared --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64
 make -j8
 sudo make install
 sudo ldconfig
